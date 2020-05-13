eaa65bebdc12b4abbaec36372861d66fbfb8c989
c-bebop <2944106+c-bebop@users.noreply.github.com>
2020-05-13 22:40:51 +0200

    First implementation of APD.

--- include/networkit/algebraic/algorithms/AlgebraicAPD.hpp
+++ include/networkit/algebraic/algorithms/AlgebraicAPD.hpp
@@ -43,12 +43,64 @@ public:
         return Matrix(a.numberOfRows(), a.numberOfColumns(), entries);
     }
 
+    bool all_nodes_reached(Matrix a)
+    {
+        bool reached = true;
+        for (count i = 0; i < a.numberOfRows() && reached; ++i)
+        {
+            for (count j = 0; j < a.numberOfColumns() && reached; ++j)
+            {
+                if (i != j)
+                {
+                    reached = a(i, j) == 1;
+                }
+            }
+        }
+        return reached;
+    }
+
+    Matrix make_distance(Matrix d_, Matrix s, Matrix z)
+    {
+        std::vector<double> entries(d_.numberOfRows() * d_.numberOfColumns(), 0.);
+        for (count i = 0; i < d_.numberOfRows(); ++i)
+        {
+            for (count j = 0; j < d_.numberOfColumns(); ++j)
+            {
+                if (i != j)
+                {
+                    entries[i * d_.numberOfRows() + j] = [&]() {
+                        if (s(i, j) >= (d_(i, j) * z(i, i)))
+                        {
+                            return 2 * d_(i, j);
+                        }
+
+                        if (s(i, j) < (d_(i, j) * z(i, i)))
+                        {
+                            return 2 * d_(i, j) - 1.;
+                        }
+
+                        return 0.;
+                    }();
+                }
+            }
+        }
+        return Matrix(d_.numberOfRows(), d_.numberOfColumns(), entries);
+    }
+
     Matrix apd(Matrix a)
     {
         Matrix z = a * a;
         Matrix a_ = the_new_a(a, z);
 
-        return a_;
+        if (all_nodes_reached(a_))
+        {
+            return a_ * 2 - a;
+        }
+
+        Matrix d_ = apd(a_);
+        Matrix s = a * d_;
+
+        return make_distance(d_, s, z);
     }
 
     Matrix distance;
